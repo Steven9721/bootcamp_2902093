d78706c2cf04f9f6253d9bc99974fce5b7f685f3
Steven <Aprendiz@BOGDFPCGMF3255.SENA.RED>
2024-10-16 12:03:06 -0500

    bootscamp controller

--- src/bootcamp.controller.ts
+++ src/bootcamp.controller.ts
@@ -1,9 +1,25 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
 
 @Controller('bootcamp')
 export class BootcampController {
     @Get()
     getAllBootcamps(): string{
         return "aqui se mostraran todos los bootcamps"
+    }
+    @Get()
+    getBootcampById(@Param("id") id:string) :string{
+        return `Aqui se va a mostrar el bootcamp con id ${id}`
+    } 
+    @Post()
+    createBootcamp(): string{
+        return `Aqui se crearan bootcamps`
+    }
+    @Put(":id")
+    updateBootcamp(@Param("id") id: string) : string {
+        return `Aqui se actualiza el bootcamp ${id}`
+    }
+    @Delete(":id")
+    deleteBootcamp(@Param("id") id: string) :string {
+        return `Aqui se borrara el bootcamp ${id}`
     }
 }
